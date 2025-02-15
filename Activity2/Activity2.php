cdb27e6a448b51e14b41151376e61282d4bf8447
Juliana R. Mancera <mancerajuliana98@gmail.com>
2025-02-15 18:05:09 +0800

    App Dev Bootstrap

---
+++ Activity2/Activity2.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+
+<!--Juliana R. Mancera 3BSCS-2 | Activity 2-->
+<lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CCC321-18/CCL321-18</title>
+    </head>
+    <body>
+        <h1>Acivtity 2</h1>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+    <div class="container-fluid">
+    
+    <a href="#">Messages<span class="badge">10</span></a><br/>
+    <a href="#">Notification<span class="badge">30</span></a>
+
+    <button class="btn btn-primary">Click to see notification<span class="badge">500</span></button>
+    <div class="page-header">
+        <h3>Download<span class="label label-default">Version 10.1</span></h3>
+    </div>
+    </div>
+
+    <br/>
+    <br/>
+    <hr/>
+
+    <div class="container-fluid">
+        <div class="alert alert-danger fade in"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already Exist!!</div>
+        <div class="alert alert-success"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already Exist!!</div>
+        <div class="alert alert-warning"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already Exist!!</div>
+        <div class="alert alert-info"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already Exist!!</div>
+    </div>
+
+
+    <br/>
+    <br/>
+    <hr/>
+
+    
+    <div class="container-fluid">
+
+        <div class="jumbotron">
+            <h1>Heading in Jumbotron</h1>
+        </div>
+        <h1>Heading without Jumbotron</h1>
+        <div class="page-header">
+            <h1>Welcome</h1>        
+        </div>
+
+        <p class="well well-sm">This is the well</p>
+        <p class="well">This is the well</p>
+        <p class="well well-lg">This is the well</p>
+    </div>
+
+    <br/> 
+    <br/>
+    <hr/>
+
+    <div class="container-fluid">
+        <span class="glyphicon glyphicon-trash"></span>:Trash Can <br/>
+        <span class="glyphicon glyphicon-pencil"></span>:Pencil <br/>
+        <button class="btn btn-success"><span class="glyphicon glyphicon-search"></span> Find</button>
+
+    </div>
+    <br/>
+    <br/>
+    <hr/>
+
+    <div class="container-fluid">
+        <div class="dropdown">
+            <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown">College<span class="caret"></span></button>
+            <ul class="dropdown-menu">
+                <li><a href="#">CSS</a></li>
+                <li><a href="#">CET</a></li>
+                <li><a href="#">CBA</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Exit</li>
+                <li><a href="#">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+
+
+    <div class="dropdown">
+            <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown">College<span class="caret"></span></button>
+            <ul class="dropdown-menu dropdown-menu-right">
+                <li><a href="#">CSS</a></li>
+                <li><a href="#">CET</a></li>
+                <li><a href="#">CBA</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Exit</li>
+                <li><a href = "#">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="dropup">
+            <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown">College<span class="caret"></span></button>
+            <ul class="dropdown-menu">
+                <li><a href="#">CSS</a></li>
+                <li><a href="#">CET</a></li>
+                <li><a href="#">CBA</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Exit</li>
+                <li><a href="#">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+
+
+    <br/>
+    <br/>
+    <hr/>
+
+    <div class="containter-fluid">
+        <button class="btn btn-default">Click me</button>
+        <button class="btn btn-primary btn-xs">Click me</button>
+        <button class="btn btn-info btn-lg">Click me</button>
+        <button class="btn btn-success btn-md">Click me</button> <!--default button-->
+        <button class="btn btn-warning">Click me</button>
+        <button class="btn btn-danger btn-sm">Click me</button>
+        <button class="btn btn-link">Click me</button>
+        <button class="btn btn-danger btn-block">Click me</button>
+        <button class="btn btn-danger disabled">Click me</button>
+        <button class="btn btn-danger active">Click me</button>
+
+    </div>
+
+    <div class="btn-group">
+
+        <button class = "btn btn-info">Home</button>
+        <button class = "btn btn-info">About Us</button>
+        <button class = "btn btn-info">Contant Us</button>
+        <button class = "btn btn-info">Products</button>
+
+    </div>
+
+    <div class="btn-group-vertical">
+
+        <button class = "btn btn-info">Home</button>
+        <button class = "btn btn-info">About Us</button>
+        <button class = "btn btn-info">Contant Us</button>
+        <button class = "btn btn-info">Products</button>
+
+    </div>
+
+    <div class="btn-group btn-group-justified">
+        <div class="btn-group">
+            <button class="btn btn-success">Home</button>
+        </div>
+        <div class="btn-group">
+            <button class="btn btn-success">About Us</button>
+        </div>
+        <div class="btn-group">
+            <button class="btn btn-success">Contact Us</button>
+        </div>
+        <div class="btn-group">
+            <button class="btn btn-success">Products</button>
+        </div>
+
+
+    </div>
+    <hr/>
+    <br/>
+
+    <class="container-fluid">
+    <img src="pic/noo.jpg">
+    <img src="pic/noo.jpg" class="img-rounded">
+    <img src="pic/noo.jpg" class="img-circle">
+    <img src="pic/noo.jpg" class="img-thubnail">        
+    
+</div>
+
+    </body>
+
+</head>
+</html>
