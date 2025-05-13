b2aac49d471a7c95721e1dfed74bcec1b1d4eb74
Juliana R. Mancera <mancerajuliana98@gmail.com>
2025-05-13 11:30:46 +0800

    portfolio demo

--- Projects/Portfolio.php
+++ Projects/Portfolio.php
@@ -36,7 +36,7 @@
                 <li><a href="#home">Home</a></li>
                 <li><a href="#work">What I do</a></li>
                 <li><a href="#about">What about me</a></li>
-                <li><a href="#contact">Contaact Me</a></li>
+                <li><a href="#contact">Contact Me</a></li>
             </ui>
 
         </nav>
@@ -47,7 +47,7 @@
         </section>
         <section id="work">
             <div class="page-header">
-                <h1 class="text-center">My Work</h1>
+                <h1 class="text-center">My Works</h1>
             </div>
             <div class="text-center">
                 <img src="pic/profile.jpg" class="img-thumbnail"/>
@@ -75,7 +75,6 @@
             </div>
             <br/>
 
-
             <div class="row">
                 <div class="col-md-6">
                     <div class="panel panel-primary">
@@ -92,10 +91,46 @@
                     </div>
                 </div>
 
-            </div>
+                <div class="row">
+                <div class="col-md-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h4>Web Designer</h4>
+                        </div>
+                        <div class="panel-body">
+                        <img src="pic/wiee.jpg" class="img-circle center-block">
+                        <p class="lead text-center">I do CSS Bootstrap Designing</p>
 
+                        </div>
+                        <div class="panel-footer"></div>
+
+                    </div>
+                </div>
+            </div>
         </section>
 
+        <section id="contact" style="background: silver">
+            <div class="page-header">
+                <h2 class="text-center">Contact Me</h2>
+            </div>
+                <form class="col-md-4 col-md-offset-4">
+                <div class="form-group">
+                    <input class="form-control" placeholder="Enter your name" type="text"/>
+                </div>
+                 <div class="form-group">
+                    <input class="form-control" placeholder="Enter your e-mail" type="email"/>
+                </div>
+                  <div class="form-group">
+                    <input class="form-control" placeholder="Subject" type="email"/>
+                </div>
+                  <div class="form-group">
+                    <textarea class="form-control rows=10"> Comments </textarea>
+                </div>
+                  <div class="form-group">
+                    <input class="btn btn-success btn-block" type="submit"/>
+                </div>
+            </form>
+        </section>
 
     </body>
 </html>
